ffca0e39efa8a969b645e82c87cb6202e2e31dcc
Dadanon <mar4ello19922@gmail.com>
2020-08-19 16:52:53 +0300

    Changes in permissions

--- api/views.py
+++ api/views.py
@@ -1,14 +1,17 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 
+from .permissions import IsAuthorOrReadOnly
 from .serializers import PostSerializer
 from posts.models import Post
 
 
 class PostList(generics.ListCreateAPIView):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly, )
     queryset = Post.objects.all()
     serializer_class = PostSerializer
